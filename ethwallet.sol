aa04dd07d55556c597466b0d1f367bf15b167b7c
VinGeo1 <123502241+VinGeo1@users.noreply.github.com>
2023-01-25 18:04:11 -0500

    Create ethwallet.sol

---
+++ ethwallet.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.17;
+
+contract EtherWallet{
+
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function receiveEther() public payable{}
+
+    function withdraw(uint _amount) public {
+        require(msg.sender == owner,"The Function caller is not the wallet owner!");
+        payable(msg.sender).transfer(_amount);
+    }
+    function getbalance() public view returns(uint){
+        return address(this).balance;
+    }
+}
